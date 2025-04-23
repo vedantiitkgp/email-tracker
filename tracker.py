65bfc83dfab89da53d46becab951a6c07e9a1549
vedantiitkgp <vedantntpc@gmail.com>
2025-04-23 08:37:33 -0700

    refiled the changes

--- tracker.py
+++ tracker.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, make_response
+from flask import Flask, request, send_file
 from datetime import datetime
 import os
 
@@ -10,11 +10,7 @@ def tracker():
     with open("opens.log", "a") as f:
         f.write(f"{datetime.now()} - Opened by {email}\n")
     print(f"{datetime.now()} - Email opened by {email}")
-    response = make_response(send_file("tracker.gif", mimetype="image/gif"))
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
-    return response
+    return send_file("tracker.gif", mimetype="image/gif")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
